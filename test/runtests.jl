6aebbfa61b4be75f780a7b59f7ffca35d41444f5
Kai Xu <me@xuk.ai>
2023-09-03 22:40:31 -0400

    test: add initial tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using RandomNames
+using RandomNames: formatword
 using Test
 
 @testset "RandomNames.jl" begin
-    # Write your tests here.
+    @test formatword("hello world") == "hello_world"
+
+    strlist = ["x", "x-y", "x-y-", "x-y-z", "x-y-1"]
+    @test countword(strlist) == 1
+    @test countword(strlist; allowindex=true) == 2
 end
